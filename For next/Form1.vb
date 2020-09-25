7cfd25de3fc54c1a6a5da0085edd2f6bc12073bf
Felix murimi <61313608+Felix280murimi@users.noreply.github.com>
2020-09-25 13:19:33 +0300

    Add project files.

---
+++ For next/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim i As Integer
+        Dim numsum As Integer
+        numsum = 0
+        For i = 1 To 10 Step 2
+            numsum = numsum + i
+        Next i
+        MessageBox.Show("The sum of the Even integr in the range 1 to " & 1 - 1 & "is" & numsum)
+        MessageBox.Show("Enjoy life in the WORLD OF NUMBERS")
+    End Sub
+End Class
